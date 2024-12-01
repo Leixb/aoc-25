2483702eb526190fd9dcfb50dce646c832392a1f
Aleix Boné <aleix.boneribo@bsc.es>
2024-12-01 14:41:42 +0100

    Day1 cleanup

--- day1.hs
+++ day1.hs
@@ -1,23 +1,18 @@
 module Main where
 
-import Data.List
 import Control.Arrow
+import Data.List
 import Data.Map
-import qualified Data.Map as M
 
-parse :: IO [[Int]]
 parse = transpose . fmap (fmap read . words) . lines <$> getContents
 
-part1 :: [[Int]] -> Int
+part1, part2 :: [[Int]] -> Int
 part1 [a, b] = sum $ abs <$> zipWith (-) (sort a) (sort b)
 part1 _ = undefined
 
-part2 :: [[Int]] -> Int
-part2 [a, b] = sum a'
-    where
-        a' = zipWith (*) a $ fmap (flip (M.findWithDefault 0) b') a
-        b' = fromListWith (+) $ fmap (,1) b
+part2 [a, b] = sum $ zipWith (*) a $ fmap (flip (findWithDefault 0) b') a
+  where
+    b' = fromListWith (+) $ fmap (,1) b
 part2 _ = undefined
 
-main :: IO ()
 main = parse >>= print . (part1 &&& part2)
