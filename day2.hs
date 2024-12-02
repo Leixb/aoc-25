22a67f07f20ae2bb60ac22306812888dc1b9f29e
Aleix Boné <aleix.boneribo@bsc.es>
2024-12-02 14:16:26 +0100

    day2 part2

--- day2.hs
+++ day2.hs
@@ -1,11 +1,16 @@
-main = getContents >>= print . part1 . parse
+import Control.Arrow
+
+main = getContents >>= print . (part1 &&& part2) . parse
 
 parse = fmap (fmap read . words) . lines
 
-part1 = length . filter id . fmap checkLine
-checkLine l = (pos || neg) && all (between 1 3 . abs) l'
+part1 = length . filter id . fmap isSafe
+part2 = length . filter id . fmap (any isSafe . removeOne)
+
+isSafe l = all (between 1 3) l' || all (between (-3) (-1)) l'
   where
     l' = zipWith (-) l (tail l)
-    neg = all (<0) l'
-    pos = all (>0) l'
     between a b n = a <= n && n <= b
+
+removeOne [] = []
+removeOne (x:xs) = xs : fmap (x :) (removeOne xs)
