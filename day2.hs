5a80d7c1efe66f751701658bfbbf50ae262b4be4
Aleix Boné <aleix.boneribo@bsc.es>
2024-12-02 18:26:02 +0100

    day2 cleanup

--- day2.hs
+++ day2.hs
@@ -1,16 +1,14 @@
 import Control.Arrow
+import Control.Monad
 
-main = getContents >>= print . (part1 &&& part2) . parse
+main = getContents >>= print . (part1 &&& part2) . fmap (fmap read . words) . lines
 
-parse = fmap (fmap read . words) . lines
+part1 = length . filter isSafe
+part2 = length . filter (any isSafe . removeOne)
 
-part1 = length . filter id . fmap isSafe
-part2 = length . filter id . fmap (any isSafe . removeOne)
-
-isSafe l = all (between 1 3) l' || all (between (-3) (-1)) l'
-  where
-    l' = zipWith (-) l (tail l)
-    between a b n = a <= n && n <= b
+isSafe = ap (zipWith (-)) tail >>> (all (between 1 3) &&& all (between (-3) (-1))) >>> uncurry (||)
+ where
+  between a b = (a <=) &&& (<= b) >>> uncurry (&&)
 
 removeOne [] = []
-removeOne (x:xs) = xs : fmap (x :) (removeOne xs)
+removeOne (x : xs) = xs : fmap (x :) (removeOne xs)
