39864dd7b7f0131e934877456e96301bfc36736d
Aleix Boné <aleix.boneribo@bsc.es>
2024-12-02 10:01:19 +0100

    Add flake shell

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, }:
+
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShell = pkgs.mkShellNoCC {
+        name = "haskell";
+
+        buildInputs = with pkgs; [
+            ghc
+            haskell-language-server
+        ];
+
+      };
+    }
+  );
+}
