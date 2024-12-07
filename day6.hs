b8b07dd02aeb152d78f8d3d6dbd665309b778125
Aleix Boné <aleix.boneribo@bsc.es>
2024-12-07 01:28:27 +0100

    day6 part2

--- day6.hs
+++ day6.hs
@@ -7,10 +7,11 @@ import Data.Array.Unboxed
 import Data.List
 import Data.Maybe
 import Data.Set qualified as S
+import Debug.Trace
 
 type Pos = (Int, Int)
 type Grid = UArray Pos Char
-data Dir = N | E | S | W deriving (Show)
+data Dir = N | E | S | W deriving (Show, Ord, Eq)
 
 type Problem = RWS Grid (S.Set Pos) (Pos, Dir)
 
@@ -43,8 +44,64 @@ step = do
     g <- ask
     let next = move d p
     tell $ S.singleton p
-    when (inRange (bounds g) next) $ put ( if g ! next == '#' then (p, rot d) else (next, d)) *> step
+    when (inRange (bounds g) next) $
+        modify
+            ( if g ! next == '#'
+                then second rot
+                else first $ const next
+            )
+            *> step
 
-part1 = S.size . snd . (evalRWS step <*> (fromJust . getStart))
+step' :: (Pos, Dir) -> Problem' ()
+step' start = do
+    (p, d) <- gets fst
+    seen <- gets snd
+    g <- ask
+    let next = move d p
 
-main = getContents >>= print . part1 . parse
+    when (inRange (bounds g) next) $ do
+        if g ! next == '#'
+            then modify . first . second $ rot
+            else do
+                prev <- get
+
+                put (start, S.empty)
+                ok <- local (//[(next, '#')]) checkLoop
+                when ok $ tell $ S.singleton next
+
+                put prev
+
+                modify . first . first $ const next
+        modify . second $ S.insert (p, d)
+        step' start
+
+checkLoop :: Problem' Bool
+checkLoop = do
+    (p, d) <- gets fst
+    seen <- gets snd
+    g <- ask
+    let next = move d p
+
+    if inRange (bounds g) next
+        then
+            if S.member (p, d) seen
+                then return True
+                else
+                    modify
+                        ( ( if g ! next == '#'
+                                then second rot
+                                else first $ const next
+                          )
+                            *** S.insert (p, d)
+                        )
+                        *> checkLoop
+        else return False
+
+type Problem' = RWS Grid (S.Set Pos) ((Pos, Dir), S.Set (Pos, Dir))
+
+part1 = S.size . snd . (evalRWS step <*> fromJust . getStart)
+part2 g = S.size . snd $ evalRWS (step' start) g (start,S.empty)
+    where
+        start = fromJust $ getStart g
+
+main = getContents >>= print . (part1 &&& part2) . parse
