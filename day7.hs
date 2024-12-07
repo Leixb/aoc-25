f66fce48a49605d239fc056cd6845d917fbbc06a
Aleix Boné <aleix.boneribo@bsc.es>
2024-12-07 11:55:16 +0100

    day7 part2

--- day7.hs
+++ day7.hs
@@ -5,11 +5,22 @@ import Data.Char
 import Text.ParserCombinators.ReadP
 
 numP = read <$> munch1 isDigit
-parse = fst . last . readP_to_S (endBy ((,) <$> (numP <* string ": ") <*> sepBy numP (char ' ')) (char '\n'))
+parse = endBy ((,) <$> (numP <* string ": ") <*> sepBy numP (char ' ')) (char '\n')
 
 valid n [m] = m == n
-valid n (x : xs) = n > 0 && valid (n - x) xs || ((n `mod` x) == 0 && valid (n `div` x) xs)
+valid n (x : xs) = n > 0 && valid (n - x) xs || (n `mod` x) == 0 && valid (n `div` x) xs
 
 part1 = sum . fmap fst . filter (uncurry valid . second reverse)
 
-main = getContents >>= print . part1 . parse
+concatNum r = (+r) . (* 10 ^ digits r)
+    where
+        digits = succ . floor . logBase 10 . fromIntegral
+
+allPossible [n] = [n]
+allPossible (x:xs) = ((x+) <$> rest) ++ ((x*) <$> rest) ++ (concatNum x <$> rest)
+    where
+        rest = allPossible xs
+
+part2 = sum . fmap fst . filter (uncurry elem . second (allPossible . reverse))
+
+main = getContents >>= print . (part1 &&& part2) . fst . last . readP_to_S parse
