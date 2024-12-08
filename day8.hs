81cc57d674657b881e0de892b9cc114302de98f4
LeixB <abone9999@gmail.com>
2024-12-08 12:47:12 +0100

    day8 cleanup

--- day8.hs
+++ day8.hs
@@ -7,39 +7,37 @@ import Data.Map qualified as M
 
 type Pos = [Int]
 
-parse :: String -> ([(Char, Pos)], Pos)
-parse s = ([(c, [i, j]) | i <- [0 .. n], j <- [0 .. m], c <- [l !! i !! j], c /= '.'], [n, m])
+parse :: String -> (Pos, [(Char, Pos)])
+parse s = ([n, m], [(c, [i, j]) | i <- [0 .. n], j <- [0 .. m], c <- [l !! i !! j], c /= '.'])
   where
     l = lines s
     n = pred $ length $ head l
     m = pred $ length l
 
-getMap :: [(Char, Pos)] -> M.Map Char [Pos]
-getMap = M.fromListWith (++) . fmap (second pure)
+buildMap :: [(Char, Pos)] -> M.Map Char [Pos]
+buildMap = M.fromListWith (++) . fmap (second pure)
 
-allPairs l = [antinodes x y | (x : xs) <- tails l, y <- xs]
+allPairs :: [Pos] -> [(Pos, Pos)]
+allPairs l = [(x, y) | (x : xs) <- tails l, y <- xs]
 
 add = zipWith (+)
 sub = zipWith (-)
 
 antinodes :: Pos -> Pos -> [Pos]
 antinodes a b = [a `sub` ab, b `add` ab]
-    where
-        ab = b `sub` a
+  where
+    ab = b `sub` a
 
+inBounds [x', y'] [x, y] = x >= 0 && y >= 0 && x <= x' && y <= y'
 
-inBounds [x', y'] [x, y]  = x >= 0 && y >= 0 && x <= x' && y <= y'
+antinodes' :: Pos -> Pos -> Pos -> [Pos]
+antinodes' l a b = al ++ bl
+  where
+    ab = b `sub` a
+    al = takeWhile (inBounds l) $ iterate (`sub` ab) a
+    bl = takeWhile (inBounds l) $ iterate (`add` ab) b
 
-antinodesL :: Pos -> Pos -> Pos -> [Pos]
-antinodesL l a b = al ++ bl
-    where
-        ab = b `sub` a
-        al = takeWhile (inBounds l) $ iterate (`sub` ab) a
-        bl = takeWhile (inBounds l) $ iterate (`add` ab) b
+part1 l = length . nub . filter (inBounds l) . concat . M.elems . fmap (allPairs >=> uncurry antinodes)
+part2 l = length . nub . concat . M.elems . fmap (allPairs >=> uncurry (antinodes' l))
 
-allPairs' l v = [antinodesL l x y | (x : xs) <- tails v, y <- xs]
-
-part1 m l = length . nub . filter (inBounds l) . concat . concat . M.elems . fmap allPairs . getMap $ m
-part2 m l = length . nub . concat . concat . M.elems . fmap (allPairs' l) . getMap $ m
-
-main = getContents >>= print . uncurry part2  . parse
+main = getContents >>= print . (uncurry part1 &&& uncurry part2) . second buildMap . parse
