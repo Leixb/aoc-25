0ab1f75cebbcea5d78de240a1036b4d1d5f39486
Aleix Boné <aleix.boneribo@bsc.es>
2024-12-09 15:06:07 +0100

    day9 part2

--- day9.hs
+++ day9.hs
@@ -2,11 +2,14 @@
 
 module Main where
 
+import Control.Applicative
+import Control.Arrow
 import Control.Monad
 import Control.Monad.ST
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
 import Data.Array.ST
 import Data.Array.Unboxed
-import Data.Bifunctor
 import Data.Char
 import Data.List
 import Data.Maybe
@@ -19,8 +22,7 @@ expand = foldMap (uncurry $ flip replicate)
 
 process l = runSTArray $ do
     arr <- newListArray (1, length l) l
-    (idxL, idxR) <- getBounds arr
-    go arr idxL idxR
+    getBounds arr >>= uncurry (go arr)
   where
     go arr iL iR = do
         (iL', iR') <- advance arr (iL, iR)
@@ -45,6 +47,55 @@ advance arr (h, t) = (,) <$> advanceHead arr h <*> advanceTail arr t
             Nothing -> advanceTail arr (pred i)
             _ -> return i
 
-checkSum = sum . zipWith (*) [0..] . catMaybes . elems
+checksum = sum . zipWith (*) [0 ..]
 
-main = getContents >>= print . checkSum . process . expand . parse
+process2 l = runSTArray $ do
+    let idxs = scanl' (+) 1 $ snd <$> l
+        iR = last idxs
+    arr <- newArray (1, iR) Nothing
+    forM_ (zip idxs l) $ \(i, v) -> writeArray arr i (Just v)
+
+    runMaybeT $ go arr iR
+
+    return arr
+  where
+    go :: MArr s -> Int -> MaybeT (ST s) ()
+    go arr iR = do
+        (i, sz) <- findVal arr iR
+        (findGap arr sz 1 >>= move arr i) <|> return ()
+        go arr $ pred i
+
+type MArr s = STArray s Int (Maybe (Maybe Int, Int))
+
+findGap :: MArr s -> Int -> Int -> MaybeT (ST s) Int
+findGap arr n i = do
+    mx <- lift $ snd <$> getBounds arr
+    guard $ i <= mx
+    ( do
+            Just (Nothing, v) <- lift (readArray arr i)
+            guard $ v >= n
+            hoistMaybe $ Just i
+        )
+        <|> findGap arr n (succ i)
+
+findVal :: MArr s -> Int -> MaybeT (ST s) (Int, Int)
+findVal arr i = do
+    guard $ i >= 1
+    lift (readArray arr i) >>= \case
+        Just (Just _, sz) -> hoistMaybe $ Just (i, sz)
+        _ -> findVal arr $ pred i
+
+move arr iVal iGap = do
+    guard $ iGap < iVal
+
+    Just (Nothing, gap) <- lift $ readArray arr iGap
+    v@(Just (Just _, sz)) <- lift $ readArray arr iVal
+    lift . writeArray arr iVal $ Just (Nothing, sz)
+    lift $ writeArray arr iGap v
+
+    when (gap > sz) . lift . writeArray arr (iGap + sz) $ Just (Nothing, gap - sz)
+
+part1 = checksum . catMaybes . elems . process . expand
+part2 = checksum . fmap (fromMaybe 0) . expand . catMaybes . elems . process2
+
+main = getContents >>= print . (part1 &&& part2) . parse
