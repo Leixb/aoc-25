2ecd28b579e606ff8ec377d6870c6e8ebe7e25a2
Aleix Boné <aleix.boneribo@bsc.es>
2024-12-11 12:02:00 +0100

    Day 11 part 2

--- day11.hs
+++ day11.hs
@@ -1,13 +1,40 @@
 module Main where
 
-blink :: Int -> [Int]
-blink n
-    | n == 0 = [1]
-    | even digits = [ l, r ]
-    | otherwise = [n * 2024]
+import Control.Arrow
+import Data.Monoid
+
+data Tree a = Tree (Tree a) a (Tree a)
+
+-- https://stackoverflow.com/questions/3208258
+memo1 f = index nats
   where
-    digits = succ . floor . logBase 10 . fromIntegral $ n
-    (l, r) = n `divMod` (10 ^ (digits `div` 2))
+    nats = go 0 1
+    go i s = Tree (go (i + s) s') (f i) (go (i + s') s')
+      where
+        s' = 2 * s
+    index (Tree l v r) i
+        | i < 0 = f i
+        | i == 0 = v
+        | otherwise = case (i - 1) `divMod` 2 of
+            (i', 0) -> index l i'
+            (i', 1) -> index r i'
 
+memo2 f = memo1 (memo1 . f)
 
-main = getContents >>= print . length . (!! 25) . iterate (concatMap blink)  . fmap read . words
+blink = memo2 blink'
+  where
+    blink' c n
+        | c == 0 = 1
+        | n == 0 = blink c' 1
+        | even digits = blink c' l <> blink c' r
+        | otherwise = blink c' $ n * 2024
+      where
+        digits = succ . floor . logBase 10 . fromIntegral $ n
+        (l, r) = n `divMod` (10 ^ (digits `div` 2))
+        c' = pred c
+
+doBlinks n = getSum . mconcat . fmap (blink n)
+part1 = doBlinks 25
+part2 = doBlinks 75
+
+main = getContents >>= print . (part1 &&& part2) . fmap read . words
