7ff6a81a6121a744f38863e8d5d73187695ba0da
LeixB <abone9999@gmail.com>
2024-12-22 13:41:57 +0100

    day22 part2

--- day22.hs
+++ day22.hs
@@ -2,14 +2,23 @@ module Main where
 
 import Control.Arrow
 import Data.Bits
+import Data.List
+import Data.Map qualified as M
 
-toSecret :: Int -> Int
-toSecret = step1 >>> step2 >>> step3
-  where
-    step1 n = ((n `shiftL` 06) `xor` n) .&. 0xFFFFFF
-    step2 n = ((n `shiftR` 05) `xor` n) .&. 0xFFFFFF
-    step3 n = ((n `shiftL` 11) `xor` n) .&. 0xFFFFFF
+parse = fmap (secretNums . read) . lines
 
-secretNums = iterate toSecret
+secretNums :: Int -> [Int]
+secretNums = take 2001 . iterate (step1 >>> step2 >>> step3)
+ where
+  step1 n = ((n `shiftL` 06) `xor` n) .&. 0xFFFFFF
+  step2 n = ((n `shiftR` 05) `xor` n) .&. 0xFFFFFF
+  step3 n = ((n `shiftL` 11) `xor` n) .&. 0xFFFFFF
 
-main = getContents >>= print . sum . fmap ((!! 2000) . secretNums . read) . lines
+part1 = sum . fmap last
+part2 = maximum . M.elems . M.unionsWith (+) . fmap (deltas . fmap (`mod` 10))
+
+deltas l = M.fromListWith (\_ x -> x) $ zip (zip4 diffs (tail diffs) (drop 2 diffs) (drop 3 diffs)) (drop 4 l)
+ where
+  diffs = zipWith (-) (tail l) l
+
+main = getContents >>= print . (part1 &&& part2) . parse
