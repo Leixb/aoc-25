be62dfa8be51d5f50fedb744b57cc505d767ebfc
LeixB <abone9999@gmail.com>
2024-12-24 13:33:25 +0100

    day24 part2

--- day24.hs
+++ day24.hs
@@ -1,33 +1,35 @@
 module Main where
 
+import Control.Arrow
 import Control.Monad.RWS
+import Data.Bits (shiftL)
 import Data.Char (digitToInt)
 import Data.Functor
+import Data.List
 import Data.Map qualified as M
-import Data.Maybe
+import Data.Tuple
 import Text.ParserCombinators.ReadP hiding (get)
 import Text.ParserCombinators.ReadP qualified as ReadP
-import Data.List
-import Data.Bits (shiftL)
 
 type Cable = String
-
 data Connection = And Cable Cable | Or Cable Cable | Xor Cable Cable deriving (Show)
 
-cable = count 3 ReadP.get
-eol = char '\n'
-initial :: ReadP (M.Map String Bool)
-initial = M.fromList <$> endBy ((,) <$> cable <*> (string ": " *> (toEnum . digitToInt <$> ReadP.get))) eol
-wires = M.fromList <$> endBy wire eol
-
-wire = do
-    a <- cable <* char ' '
-    op <- choice [string "AND" $> And, string "OR" $> Or, string "XOR" $> Xor]
-    b <- char ' ' *> cable
-    c <- string " -> " *> cable
-    return (c, op a b)
-
 parse = fst . last . readP_to_S ((,) <$> initial <*> (eol *> wires <* eof))
+  where
+    cable = count 3 ReadP.get
+    eol = char '\n'
+    initial :: ReadP (M.Map Cable Bool)
+    initial = M.fromList <$> endBy ((,) <$> cable <*> (string ": " *> (toEnum . digitToInt <$> ReadP.get))) eol
+    wires =
+        M.fromList
+            <$> (`endBy` eol)
+                ( do
+                    a <- cable <* char ' '
+                    op <- choice [string "AND" $> And, string "OR" $> Or, string "XOR" $> Xor]
+                    b <- char ' ' *> cable
+                    c <- string " -> " *> cable
+                    return (c, op a b)
+                )
 
 type Problem = RWS (M.Map Cable Connection) () (M.Map Cable Bool)
 
@@ -35,11 +37,10 @@ getConnection :: Connection -> Problem Bool
 getConnection (And a b) = (&&) <$> getWire a <*> getWire b
 getConnection (Or a b) = (||) <$> getWire a <*> getWire b
 getConnection (Xor a b) = xor <$> getWire a <*> getWire b
-
-xor True False = True
-xor False True = True
-xor _ _ = False
-
+  where
+    xor True False = True
+    xor False True = True
+    xor _ _ = False
 
 getWire :: Cable -> Problem Bool
 getWire cable = do
@@ -49,9 +50,25 @@ getWire cable = do
             return a
     gets (M.!? cable) >>= maybe computed return
 
-part1 :: M.Map Cable Bool -> M.Map Cable Connection -> Int
-part1 initial wiring = sum . fmap fst . filter snd . zip (iterate (`shiftL` 1) 1) $ fst $ evalRWS (mapM getWire zs) wiring initial
-  where
-    zs = filter ((=='z') . head) $ sort $ M.keys wiring
+fromBin :: [Bool] -> Int
+fromBin = sum . fmap fst . filter snd . zip (iterate (`shiftL` 1) 1)
 
-main = getContents >>= print . uncurry part1 . parse
+toBin :: Int -> [Bool]
+toBin = unfoldr (\v -> if v == 0 then Nothing else Just (first (== 1) (swap (divMod v 2))))
+
+part1 initial wiring = fst $ evalRWS (mapM getWire zs) wiring initial
+  where
+    zs = filter ((== 'z') . head) . sort $ M.keys wiring
+
+-- Check differences in bits. Use this to manually inspect the wire graph and
+-- fix it.
+part2 initial wiring = fmap fst . filter snd $ zip [0 ..] (zipWith (/=) got expect)
+  where
+    xs = fromBin . fmap (initial M.!) . filter ((== 'x') . head) . sort $ M.keys initial
+    ys = fromBin . fmap (initial M.!) . filter ((== 'y') . head) . sort $ M.keys initial
+    zs = filter ((== 'z') . head) . sort $ M.keys wiring
+
+    got = part1 initial wiring
+    expect = toBin $ xs + ys
+
+main = getContents >>= print . (fromBin . uncurry part1 &&& uncurry part2) . parse
