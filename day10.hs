15456fa6d31afb4f3e45ee2aaaab2fdd70f57287
LeixB <abone9999@gmail.com>
2024-12-25 23:48:59 +0100

    day 10 cleanup

--- day10.hs
+++ day10.hs
@@ -4,7 +4,7 @@ import Control.Arrow
 import Control.Monad.Reader
 import Data.Array.Unboxed
 import Data.List
-import qualified Data.Set as S
+import Data.Set qualified as S
 
 type Pos = (Int, Int)
 type Board = UArray Pos Char
@@ -12,33 +12,30 @@ type Prob = Reader Board
 
 parse :: String -> Board
 parse s = listArray ((1, 1), (n, m)) $ concat l
-  where
-    l = lines s
-    n = length l
-    m = length $ head l
+ where
+  l = lines s
+  n = length l
+  m = length $ head l
 
 origins :: Prob [Pos]
-origins =
-    ask >>= \board ->
-        return $ fmap fst . filter ((== '0') . snd) $ assocs board
+origins = asks $ fmap fst . filter ((== '0') . snd) . assocs
 
 moves :: Pos -> Prob [Pos]
-moves pos =
-    ask >>= \board ->
-        let curr = board ! pos
-         in return . filter ((== succ curr) . (board !)) . filter (inRange (bounds board)) $ (.+. pos) <$> deltas
-  where
-    deltas = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-    (ax, ay) .+. (bx, by) = (ax + bx, ay + by)
+moves pos = do
+  board <- ask
+  return . filter ((== succ (board ! pos)) . (board !)) . filter (inRange (bounds board)) $ (.+. pos) <$> deltas
+ where
+  deltas = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+  (ax, ay) .+. (bx, by) = (ax + bx, ay + by)
 
 followTrail :: [Pos] -> Prob [Pos]
 followTrail p = do
-    board <- ask
-    nxt <- concat <$> mapM moves p
+  board <- ask
+  nxt <- concat <$> mapM moves p
 
-    let (nines, rest) = partition ((== '9') . (board !)) nxt
+  let (nines, rest) = partition ((== '9') . (board !)) nxt
 
-    fmap (++ nines) $ if null rest then return [] else followTrail rest
+  fmap (++ nines) $ if null rest then return [] else followTrail rest
 
 solve fn = sum . fmap fn . runReader (origins >>= mapM (followTrail . pure))
 
